32e19070d85594fb0ac1a5c0b51234cf26c272b7
Maria-Lavinia Otelea <mlo@frankly.dk>
2023-02-14 09:30:22 +0100

    set up

---
+++ src/bookings/bookings.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Booking } from './entities/booking.entity';
+import { Repository } from 'typeorm';
+import { BookingDto } from './entities/create-booking.dto';
+
+@Injectable()
+export class BookingsService {
+    constructor(@InjectRepository(Booking) private bookingRepository: Repository<Booking>){}
+
+
+  async findAll(): Promise<Booking[]> {
+    return this.bookingRepository.find();
+  }
+
+  async findById(id: number): Promise<Booking> {
+    return this.bookingRepository.findOneBy({id: id});
+  }
+
+  async create(bookingDto: BookingDto): Promise<Booking>  {
+    return this.bookingRepository.save(bookingDto);
+  }
+
+  async update(id: number, bookingDto: BookingDto) {
+    // return this.photoRepository.find();
+  }
+
+  async remove(id: number) {
+    // return this.photoRepository.find();
+  }
+
+
+
+}
